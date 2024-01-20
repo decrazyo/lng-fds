d7a671f9fa5b2f3889c3e0c3106ab15b8936b683
decrazyo <decrazyo@gmail.com>
2024-01-19 23:42:48 -0600

    add keyboard detection

--- kernel/nintendo/keyboard_init.s
+++ kernel/nintendo/keyboard_init.s
@@ -15,14 +15,43 @@
 
 		;; initialize and install keyboard scanning routine
 keyboard_init:
-		ldx  #<lkf_keyb_scan
-		ldy  #>lkf_keyb_scan
-		jsr  lkf_hook_irq		; hook into system
+		; check that a keyboard is attached.
 
 		; reset the keyboard to the 0th row, 0th column.
 		lda #$05
 		sta JOYPAD1
 
+		; toggle the column select bit until we reach the keyless 10th row.
+		lda #$06
+		ldx #$04
+		ldy #18
+	-	sta JOYPAD1
+		stx JOYPAD1
+		dey
+		bne -
+
+		; check that the keyboard responds correctly to enable/disable signals.
+		lda JOYPAD2
+		and #$1e
+		eor #$1e
+		bne _printmsg ; branch on error
+		sta JOYPAD1
+		lda JOYPAD2
+		and #$1e
+		bne _printmsg ; branch on error
+
+		; truncate the start message to omit the error message.
+		sta _errormsg
+
+		; reset the keyboard to the 0th row, 0th column again.
+		lda #$05
+		sta JOYPAD1
+
+		ldx  #<lkf_keyb_scan
+		ldy  #>lkf_keyb_scan
+		jsr  lkf_hook_irq		; hook into system
+
+_printmsg:
 		ldx  #0
 	-	lda  _startmsg,x
 		beq  +
@@ -32,5 +61,8 @@ keyboard_init:
 	+	rts
 
 _startmsg:
-		.text "Family BASIC Keyboard module version 0.2"
+		.text "Family BASIC Keyboard module version 0.3"
+		.byte $0a
+_errormsg:
+		.text "No Keyboard"
 		.byte $0a,$00
